db7874e7c03775f59f5f97ce8340c21d95b918f1
Sofyan Egi Lesmana <sofyanegil@gmail.com>
2021-09-30 08:22:39 +0700

    Membuat Abstract Class

---
+++ abstract-class.php
@@ -0,0 +1,148 @@
+<?php
+
+abstract class Produk
+{
+  //Property
+  private $judul,
+    $penulis,
+    $penerbit,
+    $harga, //Private hanya bisa di akses di kelas itu sendiri
+    $diskon = 0; //Protected bisa di akses kelas itu sendiri dan turunan kelasnya
+
+  // Constructor Method
+  public function __construct($judul = "Judul", $penulis = "Penulis", $penerbit = "Penerbit", $harga = 0)
+  {
+    $this->judul = $judul;
+    $this->penulis = $penulis;
+    $this->penerbit = $penerbit;
+    $this->harga = $harga;
+  }
+
+  // Setter Getter Mehod
+  public function setJudul($judul)
+  {
+    $this->judul = $judul;
+  }
+
+  public function getJudul()
+  {
+    return $this->judul;
+  }
+
+  public function setPenulis($penulis)
+  {
+    $this->penulis = $penulis;
+  }
+
+  public function getPenulis()
+  {
+    return $this->penulis;
+  }
+
+  public function setPenerbit($penerbit)
+  {
+    $this->penerbit = $penerbit;
+  }
+
+  public function getPenerbit()
+  {
+    return $this->penerbit;
+  }
+
+  public function setHarga($harga)
+  {
+    return $this->harga = $harga;
+  }
+
+  public function getHarga()
+  {
+    return $this->harga - ($this->harga * $this->diskon / 100);
+  }
+
+  public function setDiskon($diskon)
+  {
+    $this->diskon = $diskon;
+  }
+
+  public function getDiskon()
+  {
+    return $this->diskon;
+  }
+
+  public function getLabel()
+  {
+    return "$this->penulis, $this->penerbit";
+  }
+
+  abstract function getInfoProduk();
+
+  public function getInfo()
+  {
+    $str = "{$this->judul} | {$this->getLabel()} (Rp. {$this->harga})";
+    return $str;
+  }
+}
+
+class Komik extends Produk
+{
+  public $jumlahHalaman;
+
+  public function __construct($judul = "Judul", $penulis = "Penulis", $penerbit = "Penerbit", $harga = 0, $jumlahHalaman = 0)
+  {
+    parent::__construct($judul, $penulis, $penerbit, $harga);
+    $this->jumlahHalaman = $jumlahHalaman;
+  }
+
+  public function getInfoProduk()
+  {
+    $str = "Komik : " . $this->getInfo() . " -  {$this->jumlahHalaman} Halaman";
+    return $str;
+  }
+}
+
+class Game extends Produk
+{
+  public $waktuMain;
+
+  public function __construct($judul = "Judul", $penulis = "Penulis", $penerbit = "Penerbit", $harga = 0, $waktuMain = 0)
+  {
+    parent::__construct($judul, $penulis, $penerbit, $harga);
+    $this->waktuMain = $waktuMain;
+  }
+
+  public function getInfoProduk()
+  {
+    $str = "Game : " . $this->getInfo() . " ~ {$this->waktuMain} Jam";
+    return $str;
+  }
+}
+
+class CetakInfoProduk
+{
+  public $daftarProduk = array();
+
+  public function  tambahProduk(Produk $produk)
+  {
+    $this->daftarProduk[] = $produk;
+  }
+
+  public function cetak()
+  {
+    $str = "DAFTAR PRODUK : <br>";
+    foreach ($this->daftarProduk as $p) {
+      $str .= "- {$p->getInfoProduk()} <br>";
+    }
+
+    return $str;
+  }
+}
+
+// Instansiasi objek dengan Constructor
+$produk1 = new Komik("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 100);
+$produk2 = new Game("Uncharted", "Neil Druckman", "Sony Computer", 250000, 50);
+
+
+$cetakProduk = new CetakInfoProduk();
+$cetakProduk->tambahProduk($produk1);
+$cetakProduk->tambahProduk($produk2);
+echo $cetakProduk->cetak();
